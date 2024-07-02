6a0502529689394f958a476732e9f4390f3ab957
ppdaifu <led.habit0o@icloud.com>
2024-07-03 01:34:52 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-col justify-center items-center">
+  <main class="flex flex-col justify-center items-center min-h-screen">
     <p class="text-center text-white">链接打不开，请联系客服。</p>
     <p class="text-center text-white">Link unavailable, contact us.</p>
   </main>
@@ -14,5 +14,8 @@
 }
 .dark .text-white {
   color: black;
+}
+.min-h-screen {
+  min-height: 100vh; 
 }
 </style>
