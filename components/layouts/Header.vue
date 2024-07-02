5ea3f23e67222266e9b60237c4b45d9d7d7747cf
ppdaifu <led.habit0o@icloud.com>
2024-07-02 23:40:14 +0800

    Update Header.vue

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { Ellipsis, X } from 'lucide-vue-next'
-//import { GitHubIcon } from 'vue3-simple-icons'
 import SwitchTheme from '../SwitchTheme.vue'
 
 const showMenu = ref(false)
@@ -64,16 +63,6 @@ const showMenu = ref(false)
                 href="/dashboard"
                 title="Sink Dashboard"
               >Dashboard</a>
-              <a
-                href="https://github.com/ccbikai/sink"
-                target="_blank"
-                title="Github"
-                class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:px-3 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800"
-              >
-                <GitHubIcon
-                  class="w-5 h-5 mr-1"
-                />
-                GitHub</a>
               <span class="ml-1">
                 <SwitchTheme />
               </span>
