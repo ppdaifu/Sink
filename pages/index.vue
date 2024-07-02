1358a310c78a5841052fad1ed0399b30ee92d111
ppdaifu <led.habit0o@icloud.com>
2024-07-03 01:42:51 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,14 @@
 <template>
-  <NuxtLayout name="default">
-    <main class="flex flex-col items-center justify-center flex-grow py-4">
-      <div class="text-center">
-        <p class="text-white dark:text-black">
-          链接打不开，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。
-        </p>
-        <p class="text-white dark:text-black">
-          Link unavailable, please <a :href="supportLink" target="_blank" class="underline">contact us</a>.
-        </p>
-      </div>
-    </main>
-  </NuxtLayout>
+  <main class="flex flex-col items-center justify-center flex-grow py-4">
+    <div class="text-center">
+      <p class="text-white dark:text-black">
+        链接打不开，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。
+      </p>
+      <p class="text-white dark:text-black">
+        Link unavailable, please <a :href="supportLink" target="_blank" class="underline">contact us</a>.
+      </p>
+    </div>
+  </main>
 </template>
 
 <script>
