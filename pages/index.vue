ac4fa0b9a0fd666076017828f5129462a421f10b
ppdaifu <led.habit0o@icloud.com>
2024-07-03 01:16:09 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,32 @@
 <template>
   <NuxtLayout name="default">
-    <div class="w-full h-full max-w-[600px] mx-auto flex flex-col items-center justify-center text-center">
-      <header class="w-full bg-gray-800 text-white p-4">
-        <!-- 这里是页眉的内容 -->
-      </header>
-      <main class="flex flex-col items-center justify-center flex-grow py-10">
-        <div class="text-white dark:text-black">
-          <p>链接打不开，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。</p>
-          <p>Link unavailable, please <a :href="supportLink" target="_blank" class="underline">contact us</a>.</p>
-        </div>
-      </main>
-      <footer class="w-full bg-gray-800 text-white p-4">
-        <!-- 这里是页脚的内容 -->
-      </footer>
-    </div>
+    <main class="flex flex-col items-center justify-center flex-grow py-10">
+      <div class="text-white dark:text-black">
+        <p>链接打不开，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。</p>
+        <p>Link unavailable, please <a :href="supportLink" target="_blank" class="underline">contact us</a>.</p>
+      </div>
+    </main>
   </NuxtLayout>
 </template>
 
-<script setup>
-import { ref, computed } from 'vue'
-
-const currentHost = ref(window.location.host)
-const supportLink = computed(() => `https://kefu.${currentHost}`)
+<script>
+export default {
+  head() {
+    return {
+      title: 'daifu link'
+    }
+  },
+  data() {
+    return {
+      currentHost: window.location.host
+    }
+  },
+  computed: {
+    supportLink() {
+      return `https://kefu.${this.currentHost}`
+    }
+  }
+}
 </script>
 
 <style scoped>
