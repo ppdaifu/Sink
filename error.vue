67c0c0af097f6b80233a39d9c83f572e3f59c7ec
ppdaifu <led.habit0o@icloud.com>
2024-07-03 00:27:43 +0800

    Update error.vue

--- error.vue
+++ error.vue
@@ -4,10 +4,10 @@
 <template>
   <NuxtLayout name="default">
     <div
-      class="w-full h-full max-w-[600px] mx-auto flex items-center justify-center text-center"
+      class="w-full h-full max-w-[600px] mx-auto flex items-center justify-center text-center p-4"
     >
-      <div class="text-white dark:text-black">
-        <p>当前链接已失效，请咨询客服获取新链接。</p>
+      <div class="text-lg text-gray-800 dark:text-gray-200">
+        <p class="mb-2">当前链接已失效，请咨询客服获取新链接。</p>
         <p>Link expired, please contact us.</p>
       </div>
     </div>
@@ -23,5 +23,11 @@
 }
 .dark .text-black {
   color: black;
+}
+.text-lg {
+  font-size: 1.125rem; /* 18px */
+}
+.mb-2 {
+  margin-bottom: 0.5rem; /* 8px */
 }
 </style>
