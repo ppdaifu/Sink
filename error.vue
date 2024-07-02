d1c2373e939b213468cbefe9117c6316c3eab89b
ppdaifu <led.habit0o@icloud.com>
2024-07-02 23:59:10 +0800

    Update error.vue

--- error.vue
+++ error.vue
@@ -1,13 +1,27 @@
 <script setup>
-import errorImage from './assets/images/404.svg?raw'
 </script>
 
 <template>
   <NuxtLayout name="default">
-    <a
-      class="w-full h-full max-w-[600px] mx-auto flex items-center justify-center [&_svg]:w-full"
-      href="/"
-      v-html="errorImage"
-    />
+    <div
+      class="w-full h-full max-w-[600px] mx-auto flex items-center justify-center text-center"
+    >
+      <div class="text-white dark:text-black">
+        <p>当前链接已失效，请咨询客服获取新链接。</p>
+        <p>Link expired, please contact us.</p>
+      </div>
+    </div>
   </NuxtLayout>
 </template>
+
+<style scoped>
+.text-center {
+  text-align: center;
+}
+.text-white {
+  color: white;
+}
+.dark .text-black {
+  color: black;
+}
+</style>
