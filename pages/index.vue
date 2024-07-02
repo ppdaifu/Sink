27e1b99210bd6c06bf8686e57388ce5e7f511564
ppdaifu <led.habit0o@icloud.com>
2024-07-03 01:36:10 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,44 @@
 <template>
-  <main class="flex flex-col justify-center items-center min-h-screen">
-    <p class="text-center text-white">链接打不开，请联系客服。</p>
-    <p class="text-center text-white">Link unavailable, contact us.</p>
-  </main>
+  <NuxtLayout name="default">
+    <main class="flex flex-col items-center justify-center flex-grow py-10">
+      <div class="text-white dark:text-black">
+        <p>链接打不开，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。</p>
+        <p>Link unavailable, please <a :href="supportLink" target="_blank" class="underline">contact us</a>.</p>
+      </div>
+    </main>
+  </NuxtLayout>
 </template>
 
-<style>
+<script>
+export default {
+  head() {
+    return {
+      title: 'daifu'
+    }
+  },
+  data() {
+    return {
+      supportLink: '' // Initialize as an empty string
+    }
+  },
+  mounted() {
+    // Set supportLink when component is mounted
+    this.supportLink = `https://kefu.${window.location.host}`;
+  }
+}
+</script>
+
+<style scoped>
 .text-center {
   text-align: center;
 }
 .text-white {
   color: white;
 }
-.dark .text-white {
+.dark .text-black {
   color: black;
 }
-.min-h-screen {
-  min-height: 100vh; 
+.underline {
+  text-decoration: underline;
 }
 </style>
