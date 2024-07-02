a2ab40364f500d77b26ffc32255c52a57a97c698
ppdaifu <led.habit0o@icloud.com>
2024-07-02 23:34:04 +0800

    Update Header.vue

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { Ellipsis, X } from 'lucide-vue-next'
-import { GitHubIcon } from 'vue3-simple-icons'
+//import { GitHubIcon } from 'vue3-simple-icons'
 import SwitchTheme from '../SwitchTheme.vue'
 
 const showMenu = ref(false)
