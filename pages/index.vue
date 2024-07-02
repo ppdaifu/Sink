4ed16e4fc8d89ee40c4f2941ff69bebb81b0787b
ppdaifu <led.habit0o@icloud.com>
2024-07-03 01:47:59 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
-  <main class="flex flex-col items-center justify-center flex-grow py-4">
-    <div class="text-center">
-      <p class="text-white dark:text-black">
-        链接打不开，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。
-      </p>
-      <p class="text-white dark:text-black">
-        Link unavailable, please <a :href="supportLink" target="_blank" class="underline">contact us</a>.
-      </p>
-    </div>
+  <main class="flex flex-col justify-center items-center min-h-screen">
+    <p class="text-center text-white">链接打不开，请联系客服。</p>
+    <p class="text-center text-white">Link unavailable, contact us.</p>
+    <!-- Moved this inside the NuxtLayout to avoid duplication -->
+    <NuxtLayout name="default">
+      <div class="text-white dark:text-black">
+        <p>链接打不开，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。</p>
+        <p>Link unavailable, please <a :href="supportLink" target="_blank" class="underline">contact us</a>.</p>
+      </div>
+    </NuxtLayout>
   </main>
 </template>
 
@@ -20,13 +21,12 @@ export default {
   },
   data() {
     return {
-      currentHost: window.location.host
+      supportLink: '' // Initialize as an empty string
     }
   },
-  computed: {
-    supportLink() {
-      return `https://kefu.${this.currentHost}`
-    }
+  mounted() {
+    // Set supportLink when component is mounted
+    this.supportLink = `https://kefu.${window.location.host}`;
   }
 }
 </script>
@@ -38,8 +38,14 @@ export default {
 .text-white {
   color: white;
 }
+.dark .text-white {
+  color: white; /* Fixed color styling */
+}
 .dark .text-black {
   color: black;
+}
+.min-h-screen {
+  min-height: 100vh;
 }
 .underline {
   text-decoration: underline;
