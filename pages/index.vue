910877e1f44d61336f0e9a781ff883b12b953a4d
ppdaifu <led.habit0o@icloud.com>
2024-07-03 20:23:34 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col min-h-screen justify-center items-center">
-    <main class="flex flex-col justify-center items-center flex-grow text-center p-0"> <!-- 将 p-4 改为 p-0 以减少上下内边距 -->
+    <main class="flex flex-col justify-center items-center flex-grow text-center">
       <!-- 图片放置在提示语上方 -->
-      <img src="/assets/images/404.svg" alt="404 Image" class="mb-2" /> <!-- 减少图片下方的外边距 -->
+      <img src="/assets/images/404.svg" alt="404 Image" />
       
       <!-- 显示有超链接的提示语 -->
-      <div class="text-link">
-        <p>链接无效1，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。</p>
+      <div class="text-link mt-4">
+        <p>链接无效，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。</p>
         <p>Link unavailable, please <a :href="supportLink" target="_blank" class="underline">contact us</a>.</p>
       </div>
     </main>
@@ -34,10 +34,10 @@ export default {
 .text-link {
   color: var(--text-color); /* Use a variable for text color */
 }
-.dark.text-link {
+.dark .text-link {
   color: white; /* Ensure that text is white in dark mode */
 }
-.light.text-link {
+.light .text-link {
   color: black; /* Ensure that text is black in light mode */
 }
 .min-h-screen {
