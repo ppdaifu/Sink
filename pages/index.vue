ba400674c6fb8a7b39f00386b5991dcbf92dc84e
ppdaifu <led.habit0o@icloud.com>
2024-07-03 21:00:47 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="flex flex-col min-h-screen justify-center items-center">
-    <main class="flex flex-col justify-center items-center flex-grow text-center">
-      <!-- 显示一句话提示语 -->
-      <p class="text-link">链接无效</p>
-    </main>
+  <div class="container">
+    <p class="text-link">链接无效</p>
   </div>
 </template>
 
@@ -13,33 +10,22 @@ export default {
 </script>
 
 <style scoped>
-.text-link {
-  color: black; /* Default text color for light mode */
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Full viewport height */
+  margin: 0; /* No margin */
 }
+
+.text-link {
+  color: black; /* Default text color */
+}
+
 .dark .text-link {
   color: white; /* Text color for dark mode */
 }
-.min-h-screen {
-  min-height: 100vh; 
-}
-.flex {
-  display: flex;
-}
-.flex-col {
-  flex-direction: column;
-}
-.justify-center {
-  justify-content: center;
-}
-.items-center {
-  align-items: center;
-}
-.flex-grow {
-  flex-grow: 1;
-}
-.text-center {
-  text-align: center;
-}
+
 html, body {
   height: 100%; /* Ensure the body takes the full height */
   margin: 0; /* Remove default margin */
