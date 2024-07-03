cee5294569e0594966a02acf4750cc33b9334557
ppdaifu <led.habit0o@icloud.com>
2024-07-03 19:41:03 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,9 @@
 <template>
-  <div class="flex flex-col min-h-screen">
-    <main class="flex flex-col justify-center items-center flex-grow overflow-hidden text-center">
-      <!-- 图片放置在提示语上方 -->
-      <img src="/assets/images/404.svg" alt="404 Image" class="w-1/2 md:w-1/3 mb-4" />
-      
-      <!-- 显示有超链接的提示语 -->
+  <div class="container">
+    <main class="content">
+      <img src="/assets/images/404.svg" alt="404 Image" class="image" />
       <div class="text-link">
-        <p>链接打不开，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。</p>
+        <p>链接无效，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。</p>
         <p>Link unavailable, please <a :href="supportLink" target="_blank" class="underline">contact us</a>.</p>
       </div>
     </main>
@@ -17,13 +14,12 @@
 export default {
   data() {
     return {
-      supportLink: '' // Initialize as an empty string
+      supportLink: ''
     }
   },
   mounted() {
-    // Set supportLink when component is mounted
     const domainParts = window.location.host.split('.');
-    domainParts.shift(); // Remove the first part
+    domainParts.shift(); 
     const mainDomain = domainParts.join('.');
     this.supportLink = `https://kefu.${mainDomain}`;
   }
@@ -31,32 +27,43 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  overflow: auto;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.image {
+  width: 50%;
+}
+
 .text-link {
-  color: var(--text-color); /* Use a variable for text color */
+  color: var(--text-color);
 }
 .dark .text-link {
-  color: white; /* Ensure that text is white in dark mode */
+  color: white; 
 }
 .light .text-link {
-  color: black; /* Ensure that text is black in light mode */
+  color: black; 
 }
-.min-h-screen {
-  min-height: 100vh; 
-}
-.underline {
-  text-decoration: underline;
-}
-.overflow-hidden {
-  overflow: hidden; /* Disable scrolling */
-}
+
 body {
-  --text-color: black; /* Default text color */
+  --text-color: black;
 }
 .dark body {
-  --text-color: white; /* Text color for dark mode */
+  --text-color: white; 
 }
-html, body {
-  height: 100%; /* Ensure the body takes the full height */
-  margin: 0; /* Remove default margin */
+
+.underline {
+  text-decoration: underline;
 }
 </style>
