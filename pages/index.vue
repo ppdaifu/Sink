a4d40056938fb68d4236abe523ac9b9063052098
ppdaifu <led.habit0o@icloud.com>
2024-07-03 20:48:33 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,10 @@
-<template>
-  <div class="flex flex-col min-h-screen justify-center items-center">
-    <main class="flex flex-col justify-center items-center flex-grow text-center">
-      <!-- 图片放置在提示语上方 -->
+  <template>
       <img src="/assets/images/404.svg" alt="404 Image" />
       
-      <!-- 显示有超链接的提示语 -->
       <div class="text-link mt-4">
         <p>链接无效，请<a :href="supportLink" target="_blank" class="underline">联系客服</a>。</p>
-        <p>Link unavailable, please <a :href="supportLink" target="_blank" class="underline">contact us</a>.</p>
       </div>
-    </main>
-  </div>
+
+    
 </template>
 
