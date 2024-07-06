fcfcb5bb98ff386481dad5d7fce18b15ab6e9a6d
ppdaifu <led.habit0o@icloud.com>
2024-07-06 12:23:02 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,15 +9,3 @@
     <LayoutsFooter />
   </main>
 </template>
-
-<script setup>
-if (process.client) {
-  var _hmt = _hmt || [];
-  (function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?7af9cc781ae0b1d55a016f7ae0dde530";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-  })();
-}
-</script>
