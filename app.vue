3aa01689d567fd710812d7d82955d43e9da95338
ppdaifu <led.habit0o@icloud.com>
2024-07-06 12:16:21 +0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
-<script setup>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
 const { title, description, image } = useAppConfig()
 useSeoMeta({
   title: title + ' - ' + description,
@@ -12,7 +14,6 @@ useSeoMeta({
   twitterImage: image,
   twitterCard: 'summary_large_image',
 })
-
 useHead({
   htmlAttrs: {
     lang: 'en',
@@ -24,6 +25,17 @@ useHead({
       href: '/icon-192.png',
     },
   ],
+})
+
+onMounted(() => {
+  // Baidu Analytics
+  const _hmt = (window as any)._hmt = (window as any)._hmt || [];
+  (function() {
+    const hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?7af9cc781ae0b1d55a016f7ae0dde530";
+    const s = document.getElementsByTagName("script")[0]; 
+    s.parentNode?.insertBefore(hm, s);
+  })();
 })
 </script>
 
